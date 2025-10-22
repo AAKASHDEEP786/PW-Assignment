b17454262ce7082759509d5656899f1969d6d1ed
Aakash Deep <82133913+AAKASHDEEP786@users.noreply.github.com>
2025-10-22 12:50:17 +0530

    Delete Assignment-10/my-app directory

--- Assignment-10/my-app/run-tests.js
+++
@@ -1,9 +0,0 @@
-// run-tests.js
-require('dotenv').config({ path: '.env.test' });
-const jest = require('jest');
-
-jest.run([
-  '--runInBand',
-  '--detectOpenHandles',
-  'tests/integration'
-]);
